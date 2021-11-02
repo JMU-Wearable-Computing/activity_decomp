70ff3bb0c75edf43fdc085860a5d92ddda3cb519
Riley White <rileywhite89@gmail.com>
2021-11-02 11:01:25 -0400

    Add basic setup.
    Name of package is skillest, but this doesn't match the repo name. Probably doesn't matter.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='skill-estimation',
+    version='0.1',
+    url='https://github.com/JMU-Wearable-Computing/skill-estimation',
+    author='Riley White',
+    author_email='rileywhite89@gmail.com',
+    license='MIT',
+    packages=[
+        'skillest',
+    ]
+)
