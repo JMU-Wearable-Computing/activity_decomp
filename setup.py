2e229b145ed4ab20e792dedb07e19c05d80868fd
Riley White <rileywhite89@gmail.com>
2023-03-24 16:41:34 -0400

    exclude installing joints if it exists already

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup, find_packages
 import os
 lib_folder = os.path.dirname(os.path.realpath(__file__))
 requirement_path = lib_folder + '/requirements.txt'
-install_requires = [] # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+install_requires = [] 
 if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
+
+# Try to import joints, if joints can be successfully 
+# imported then we will skip installing it.
+try: 
+    import joints
+    install_requires = [line for line in install_requires if not line.startswith("joint")]
+except:
+    pass
 
 
 setup(
