6678dba25dcf3238e926d07fd567ba9e15b0eabe
Riley White <rileywhite89@gmail.com>
2023-04-26 16:38:51 -0400

    give a preview before recording

--- capture_exercise.py
+++ capture_exercise.py
@@ -28,11 +28,15 @@ def record(subject, exercise):
         os.makedirs(DIR)
 
     window_name = "prep"
-    image = np.zeros((400, 400))
-    cv2.putText(image, "Get Ready", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 8, 255, thickness=10) 
-    cv2.imshow(window_name, image)
-    cv2.setWindowProperty(window_name, cv2.WND_PROP_TOPMOST, 1)
-    cv2.waitKey(0)
+    # image = np.zeros((1000, 1000))
+    for landmark, wlandmark, image in j.blaze.capture(window_name, return_image=True, save_video=False):
+        cv2.putText(image, "Get Ready", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 8, 255, thickness=10) 
+        cv2.imshow(window_name, image)
+        cv2.setWindowProperty(window_name, cv2.WND_PROP_TOPMOST, 1)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
     cv2.destroyWindow(window_name)
     # Hack to make sure the window closes quickly
     # Gives opencv2 more time to destroy window
@@ -55,15 +59,22 @@ def record(subject, exercise):
     for i in range(k):
         j.blaze.vizualize(activity.rules[f"pose_{i}"].landmarks, name=f"pose_{i}")
 
-    cv2.waitKey(0)
+    while True:
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
+    for i in range (4):
+        cv2.waitKey(1)
 
     decomposer = Decomposer(k=None, valid_angles=list(angles_dict.keys()), grid_search=True, reps=10)
     activity, m = decomposer.decompose(angles_dict, landmarks)
 
-    cv2.destroyAllWindows()
     for i in range(k):
         j.blaze.vizualize(activity.rules[f"pose_{i}"].landmarks, name=f"pose_{i}")
 
-    cv2.waitKey(0)
+    while True:
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
 record(args.subject, args.exercise)
